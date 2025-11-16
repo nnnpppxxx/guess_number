302ad70cc72e976978acf13d0f4ac52db67d0f0d
Valentyn Prudskyi <prudskyi@gmail.com>
2025-11-16 20:18:47 +0100

    v 1.0

---
+++ guess_number.py
@@ -0,0 +1,41 @@
+from random import *
+
+def is_valid(num):
+    if num > 100:
+        print('Число больше ста.')
+        return False
+    elif num < 0:
+        print('Число меньше нуля.')
+        return False
+    elif num == 0:
+        print('Число равно ноль.')
+        return False
+    else:
+        return True
+  
+
+cislo = randint(1, 100)
+print('Добро пожаловать в угадайку!')
+usercislo = int(input('Введите число от 1 до 100: '))
+
+count = 1
+
+
+if is_valid(usercislo):
+    while usercislo != cislo:
+        if usercislo > cislo:
+            print('Твое число больше чем загаданное')
+        else:
+            print('Твое число меньше чем загаданное')
+        usercislo = int(input('Введите число от 1 до 100: '))
+        count += 1
+else:
+    usercislo = int(input('Введите новое число от 1 до 100: '))
+
+
+if usercislo == cislo:
+    print('Угадал!')
+    print(f'Кол-во попыток: {count}')
+
+            
+        
