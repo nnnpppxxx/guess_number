25a2330196362b3197b1cb55c80674ea70191605
Valentyn Prudskyi <prudskyi@gmail.com>
2025-11-16 20:43:34 +0100

    fixed bug if num > range

--- guess_number.py
+++ guess_number.py
@@ -14,14 +14,8 @@ def is_valid(num):
         return True
   
 
-cislo = randint(1, 100)
-print('Добро пожаловать в игру угадайку!')
-usercislo = int(input('Введите число от 1 до 100: '))
-
-count = 1
-
-
-if is_valid(usercislo):
+def check_num(usercislo):
+    global count
     while usercislo != cislo:
         if usercislo > cislo:
             print('Твое число больше чем загаданное')
@@ -29,13 +23,18 @@ if is_valid(usercislo):
             print('Твое число меньше чем загаданное')
         usercislo = int(input('Введите число от 1 до 100: '))
         count += 1
+        if usercislo == cislo:
+            print('Угадал')
+            print(f'Количество попыток: {count}')
+
+
+cislo = randint(1, 100)
+print('Добро пожаловать в игру угадайку!')
+usercislo = int(input('Введите число от 1 до 100: '))
+count = 0
+if is_valid(usercislo):
+    check_num(usercislo)
 else:
-    usercislo = int(input('Введите новое число от 1 до 100: '))
-
-
-if usercislo == cislo:
-    print('Угадал!')
-    print(f'Кол-во попыток: {count}')
-
-            
-        
+    usercislo = int(input('Введите правильное число от 1 до 100: '))
+    if is_valid(usercislo):
+        check_num(usercislo)
